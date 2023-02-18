e80bf8dfcccd1ac46569472ff4c350085b2cbef8
Zheng20010712 <Zheng20010712@163.com>
2023-02-18 20:39:22 +0800

    feat: 工程化配置

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,15 @@
  * @email: zheng20010712@163.com
  * @Date: 2023-02-12 20:33:37
  * @LastEditors: ZhengXiaoRui
- * @LastEditTime: 2023-02-18 18:57:13
+ * @LastEditTime: 2023-02-18 20:04:38
  */
-import Head from "next/head";
-import Image from "next/image";
+// import Head from "next/head";
+// import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
+// import styles from "@/styles/Home.module.css";
 import RecuitmentItem from "./components/recruitmentItem";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
